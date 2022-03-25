e7cf9952f0be86134a4c7bd36c45f00b29eb1c2c
Mourathiago <moura.francisco.thiago@hotmail.com>
2022-03-25 15:01:28 -0400

    Delete user

--- src/main/java/com/mouradev/workshop_spring_mongo/resources/UserResource.java
+++ src/main/java/com/mouradev/workshop_spring_mongo/resources/UserResource.java
@@ -9,6 +9,7 @@ import com.mouradev.workshop_spring_mongo.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,5 +39,11 @@ public class UserResource {
     public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
         User user = service.insert(userDTO.fromDTO());
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri()).build();
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
