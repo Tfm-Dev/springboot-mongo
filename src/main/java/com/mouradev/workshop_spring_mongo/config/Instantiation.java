f8d6e35468596bf6ec022ca85fe88c03a7234e39
Mourathiago <moura.francisco.thiago@hotmail.com>
2022-03-25 16:47:01 -0400

    Creating a authorDTO

--- src/main/java/com/mouradev/workshop_spring_mongo/config/Instantiation.java
+++ src/main/java/com/mouradev/workshop_spring_mongo/config/Instantiation.java
@@ -6,6 +6,7 @@ import java.util.TimeZone;
 
 import com.mouradev.workshop_spring_mongo.domain.Post;
 import com.mouradev.workshop_spring_mongo.domain.User;
+import com.mouradev.workshop_spring_mongo.dto.AuthorDTO;
 import com.mouradev.workshop_spring_mongo.repository.PostRepository;
 import com.mouradev.workshop_spring_mongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-        
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
     
